5fd8df25e82e403f3ffec72361e1b103ed9fac7d
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-11-03 13:58:20 -0700

    routes roughed in

--- controllers/api/thoughtRoutes.js
+++ controllers/api/thoughtRoutes.js
@@ -1,8 +1,83 @@
 const router = require("express").Router();
+const Thought = require('../../models/thought');
 
 router.get("/", async (req, res) => {
   try {
-    res.json({ message: "hello" });
+    const thoughtData = await Thought.find({})
+
+    res.status(200).json(thoughtData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const thoughtData = await Thought.findById(req.params.id)
+
+    res.status(200).json(thoughtData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const thoughtData = await Thought.create({
+      thoughtText: req.body.thoughtText,
+      username: req.body.username
+    })
+
+    /* What is the difference between this syntax and the one above? */
+    // const thoughtData = await new Thought({
+    //  thoughtText: req.body.thoughtText,
+    //  username: req.body.username
+    // })
+    // thoughtData.save()
+
+    res.status(200).json(thoughtData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const thoughtData = await Thought.findByIdAndUpdate(req.params.id, {
+      thoughtText: req.body.thoughtText,
+    })
+    res.status(200).json(thoughtData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const thoughtData = await Thought.findByIdAndDelete(req.params.id)
+    res.status(200).json(thoughtData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// REACTION ROUTES
+
+router.post("/:id/reactions", async (req, res) => {
+  try {
+    const thoughtData = await Thought.findById(req.params.id)
+
+    res.status(200).json(thoughtData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/:id/reactions", async (req, res) => {
+  try {
+    const thoughtData = await Thought.findById(req.params.id)
+
+    res.status(200).json(thoughtData)
   } catch (err) {
     console.log(err);
   }
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,86 @@
 const router = require("express").Router();
+const User = require('../../models/user');
 
 router.get("/", async (req, res) => {
   try {
-    // const userData = await 
-    res.status(200).json({message : 'hi'})
+    const userData = await User.find({})
+
+
+    res.status(200).json(userData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await User.findById(req.params.id)
+
+    res.status(200).json(userData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email
+    })
+
+    /* What is the difference between this syntax and the one above? */
+    // const userData = await new User({
+    //   username: req.body.username,
+    //   email: req.body.email
+    // })
+    // userData.save()
+
+    res.status(200).json(userData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const userData = await User.findByIdAndUpdate(req.params.id, {
+      username: req.body.username,
+      email: req.body.email
+    })
+    res.status(200).json(userData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const userData = await User.findByIdAndDelete(req.params.id)
+    res.status(200).json(userData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// FRIEND ROUTES
+
+router.post("/:id/friends/:friendID", async (req, res) => {
+  try {
+    const userData = await User.findById(req.params.friendID)
+
+    res.status(200).json(userData)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/:id/friends/:friendID", async (req, res) => {
+  try {
+    const userData = await User.findById(req.params.friendID)
+
+    res.status(200).json(userData)
   } catch (err) {
     console.log(err);
   }
